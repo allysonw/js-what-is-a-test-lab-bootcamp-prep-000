c0ed7c89e73d880cfebc45434208a2907f2a60ae
Allyson Wesman <allysonwesman@gmail.com>
2017-12-22 21:06:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ var height = 30
 var message = `${name} is ${height} inches tall`
 
 var word
- 
+
 // assign a value to the variable
 word = 'bird'
- 
-console.log(word) // 'bird'
- 
+
+console.log(window.word) // 'bird'
+
 // assign another value to the variable
 word = 'dog'
- 
+
 console.log(word) // 'dog'
 
 module.exports = {name, height, message}
